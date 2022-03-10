48997d0c562f502524ca2e359f03bccff2704a58
AleksTMN <kirilloff_77@mail.ru>
2022-03-10 10:45:20 +0500

    2hw

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,31 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
-}
+        System.out.println("Задача1");
+        int clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+
+        System.out.println("Задача2");
+        int clientDeviceYear = 2012;
+        if (clientOS == 0 && clientDeviceYear >= 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS != 0 && clientDeviceYear >= 2015) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }
+        System.out.println("Задача3");
+        
+
+
+            }
+        }
+
+
+
